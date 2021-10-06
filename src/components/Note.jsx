25660aef04861b630a7483d2ff444086d923c0ea
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-10-06 13:00:24 +0200

    keeper-app

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h2>Note title</h2>
+      <p>Note content</p>
+    </div>
+  );
+}
+
+export default Note;
